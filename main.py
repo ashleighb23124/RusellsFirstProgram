0f85bc56e91bd28f8f52406e10feabcd3ddd5fbf
AshleighBenjami <>
2021-09-16 18:21:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Welcome to Ashleigh's Caribbean Bistro ")
+print("Please Your Dish")
+
+customer_order = input(
+    "A- Curry Chicken  B- Oxtail C- Johnny Cake D - Saltfish ")
+
+if customer_order == "A":
+    print("Here is your Curry Chicken")
+elif customer_order == "B":
+    print("Enjoy your Oxtail")
+elif customer_order == "C":
+    print("Enjoy the Johnny Cake")
+elif customer_order == "D":
+    print("Here is your Saltfish")
+else:
+  print("Uh oh, We don't have that")
